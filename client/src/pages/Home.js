1443a63661bade2eab9f4935454aeb9a497a2fb1
Narayana reddy <gurrala.gvnreddy@gmail.com>
2022-04-27 05:58:53 -0500

    Added likecount and comment buttons

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,62 @@
 import React from 'react'
 
+import { gql,useQuery } from "@apollo/client";
+import { Grid, Transition } from 'semantic-ui-react';
+
+import PostCard from '../components/PostCard';
+
+
 const Home = () => {
+
+    const {
+        loading,
+         error, data
+      } = useQuery(FETCH_POSTS_QUERY);
+
+
   return (
-    <div>Home</div>
+    <Grid columns={3}>
+    <Grid.Row className="page-title">
+      <h1>Recent Posts</h1>
+    </Grid.Row>
+    <Grid.Row>
+      {loading ? (
+        <h1>Loading posts..</h1>
+      ) : (
+        <Transition.Group>
+          {data &&
+            data.getPosts.map((post) => (
+              <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+                <PostCard post={post} />
+              </Grid.Column>
+            ))}
+        </Transition.Group>
+      )}
+    </Grid.Row>
+  </Grid>
   )
 }
+
+const FETCH_POSTS_QUERY = gql`
+  {
+    getPosts {
+      id
+      body
+      createdAt
+      username
+      likeCount
+      likes {
+        username
+      }
+      commentCount
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+    }
+  }
+`;
 
 export default Home
