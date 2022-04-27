8201ec4053b8c84c398b65eba3ac31d529d0ed4d
Narayana reddy <gurrala.gvnreddy@gmail.com>
2022-04-26 20:35:58 -0500

    Added routing and semantic UI to the App

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
 
 import './App.css';
 
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import MenuBar from './components/MenuBar'
+
 function App() {
   return (
-    <div>
-      Hello World!
-    </div>
+    <Router>
+      <Container>
+        <MenuBar />
+        <Routes>
+          <Route path="/" exact element={<Home />}></Route>
+          <Route exact path="/login" element={<Login/>} ></Route>
+          <Route exact path="/register" element={<Register/>} ></Route>
+        </Routes>
+        </Container>
+    </Router>
   );
 }
 
