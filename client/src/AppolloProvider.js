a2c4e62274eb029b3c5f1746d2f2640abe1dbcf9
Narayana reddy <gurrala.gvnreddy@gmail.com>
2022-05-30 18:36:47 -0500

    Create post page created and sending request to server to create post

--- client/src/AppolloProvider.js
+++ client/src/AppolloProvider.js
@@ -1,18 +1,32 @@
 import React from "react";
 import App from "./App";
-import {
-    ApolloClient,
-    InMemoryCache,
-    ApolloProvider,
-  } from "@apollo/client";
 
-  const client = new ApolloClient({
-    uri: 'http://localhost:4000',
-    cache: new InMemoryCache()
-  });
+import { ApolloClient, InMemoryCache, ApolloProvider,createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
-  export default (
-      <ApolloProvider client={client}>
-          <App />
-      </ApolloProvider>
-  )
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000",
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('jwtToken');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+export default (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
--- client/src/pages/AddPost.js
+++ client/src/pages/AddPost.js
@@ -1,9 +1,83 @@
-import React from 'react'
-
+import { gql, useMutation } from "@apollo/client";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button, Form } from "semantic-ui-react";
 const AddPost = () => {
-  return (
-    <div>AddPost</div>
-  )
-}
 
-export default AddPost
+  let navigate = useNavigate();
+  const [values, setValues] = useState({
+    body: "",
+  });
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+  const onSubmit = () => {
+    createpost();
+  };
+
+  const [createpost, { error }] = useMutation(CREATE_POST_MUTATION, {
+    update(_, { result }) {
+      // console.log("in success")
+    //   console.log(result);
+      //   context.login(userData);
+        
+        navigate(`/`);
+      // history.push("/");
+    },
+    onError(err) {
+      if (err.graphQLErrors) {
+        console.log("in Error");
+        console.log(err);
+        console.log(err.graphQLErrors);
+        // setErrors(err.graphQLErrors[0].extensions.errors);
+      }
+    },
+    variables: values,
+  });
+
+  return (
+    <div className="form-container">
+      <Form onSubmit={onSubmit}>
+        <h1>Create Post</h1>
+        <Form.Field>
+          <Form.Input
+            label="Create Post"
+            palceholder="Create Post"
+            name="body"
+            onChange={onChange}
+            value={values.body}
+          />
+        </Form.Field>
+        <Button type="submit" color="teal">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+const CREATE_POST_MUTATION = gql`
+  mutation Mutation($body: String!) {
+    createPost(body: $body) {
+      id
+      body
+      username
+      createdAt
+      comments {
+        id
+        createdAt
+        username
+        body
+      }
+      likes {
+        id
+        createdAt
+        username
+      }
+      likeCount
+      commentCount
+    }
+  }
+`;
+
+export default AddPost;
